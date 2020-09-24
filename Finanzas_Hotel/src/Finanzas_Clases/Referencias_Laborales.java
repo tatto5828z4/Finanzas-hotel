a0ba69bf6b6bc941edfdacfae3dc733886927fb4
Josuezapata7 <josuezapata798@gmail.com>
2020-09-24 13:08:19 -0600

    Version 2

---
+++ Finanzas_Hotel/src/Finanzas_Clases/Referencias_Laborales.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Finanzas_Clases;
+
+/**
+ *
+ * @author Langas
+ */
+public class Referencias_Laborales 
+{
+    
+}
